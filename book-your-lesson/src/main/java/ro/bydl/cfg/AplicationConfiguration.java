8aa27d313edc7f6bcb585d1487664c5ac99aab0b
Raul Ranete <ranete.raul@yahoo.com>
2016-10-03 22:20:14 +0300

    no message

--- book-your-lesson/src/main/java/ro/bydl/cfg/AplicationConfiguration.java
+++ book-your-lesson/src/main/java/ro/bydl/cfg/AplicationConfiguration.java
@@ -16,10 +16,10 @@ public class AplicationConfiguration {
 		BasicDataSource dataSource = new BasicDataSource();
 		dataSource.setDriverClassName("org.postgresql.Driver");
 
-		String url = new StringBuilder().append("jdbc:").append("postgresql").append("://").append("ec2-54-228-189-38.eu-west-1.compute.amazonaws.com")
-				.append(":").append("5432").append("/").append("d2bqaqj3ktrgeu").toString();
+		String url = new StringBuilder().append("jdbc:").append("postgresql").append("://").append("localhost")
+				.append(":").append("5432").append("/").append("bydl").toString();
 
-		dataSource.setUrl( "//eusarymvdtcbzu:pyXvv8ueit19S_m4aNbZzWuD7x@ec2-54-228-189-38.eu-west-1.compute.amazonaws.com:5432/d2bqaqj3ktrgeu");
+		dataSource.setUrl("postgres://eusarymvdtcbzu:pyXvv8ueit19S_m4aNbZzWuD7x@ec2-54-228-189-38.eu-west-1.compute.amazonaws.com:5432/d2bqaqj3ktrgeu");
 		dataSource.setUsername("eusarymvdtcbzu");
 		dataSource.setPassword("pyXvv8ueit19S_m4aNbZzWuD7x");
 		return dataSource;
