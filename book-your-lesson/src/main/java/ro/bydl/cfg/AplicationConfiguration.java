4c5365d3aee6482d677e8be6eb2a33c9fd72a31a
Raul Ranete <ranete.raul@yahoo.com>
2016-10-06 21:38:43 +0300

    for heroku

--- book-your-lesson/src/main/java/ro/bydl/cfg/AplicationConfiguration.java
+++ book-your-lesson/src/main/java/ro/bydl/cfg/AplicationConfiguration.java
@@ -14,14 +14,14 @@ public class AplicationConfiguration {
 	@Bean
 	public BasicDataSource dataSource() {
 		BasicDataSource dataSource = new BasicDataSource();
-		dataSource.setDriverClassName("org.postgresql.Driver");
+		//dataSource.setDriverClassName("org.postgresql.Driver");
 
-		String url = new StringBuilder().append("jdbc:").append("postgresql").append("://").append("localhost")
-				.append(":").append("5432").append("/").append("bydl").toString();
+		String url = new StringBuilder().append("jdbc:").append("postgresql").append("://").append("ec2-54-228-189-38.eu-west-1.compute.amazonaws.com")
+				.append(":").append("5432").append("/").append("d2bqaqj3ktrgeu").toString();
 
 		dataSource.setUrl(url);
-		dataSource.setUsername("postgres");
-		dataSource.setPassword("serpentina");
+		dataSource.setUsername("eusarymvdtcbzu");
+		dataSource.setPassword("pyXvv8ueit19S_m4aNbZzWuD7x");
 		return dataSource;
 
 	}
