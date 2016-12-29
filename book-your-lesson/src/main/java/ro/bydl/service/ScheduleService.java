3dd9a8f3f81b99732179e8eda41cafb940d59546
Raul Ranete <ranete.raul@yahoo.com>
2016-12-29 17:49:37 +0200

    no message

--- book-your-lesson/src/main/java/ro/bydl/service/ScheduleService.java
+++ book-your-lesson/src/main/java/ro/bydl/service/ScheduleService.java
@@ -1,6 +1,10 @@
 package ro.bydl.service;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Collection;
+import java.util.Date;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,6 +15,12 @@ import ro.bydl.dao.ScheduleDAO;
 import ro.bydl.domain.Schedule;
 import ro.bydl.exceptions.ValidationException;
 
+/**
+ * Service class for schedules
+ * 
+ * @author Raul
+ *
+ */
 @Service
 public class ScheduleService {
 	private static final Logger LOGGER = LoggerFactory.getLogger(ScheduleService.class);
@@ -19,34 +29,42 @@ public class ScheduleService {
 
 	ScheduleDAO dao;
 
+	/**
+	 * Returners an arrey of strings format type dd.MM.yyyy
+	 * 
+	 * @param week
+	 * @return String[] week days
+	 */
 	public String[] getDays(int week) {
 		CalendarHelper calHelp = new CalendarHelper();
 		return calHelp.getDays(week);
 	}
 
+	/**
+	 * validation method for type schedule (empty)
+	 * 
+	 * @param schedule
+	 * @throws ValidationException
+	 */
 	public void validate(Schedule schedule) throws ValidationException {
 
-		
-//	List errors= new LinkedList<>();
-//	if(isScheduleInThePast(schedule)){
-//		
-//		errors.add("you canot book lessons in the past :)");
-//	}
-
-		
 	}
 
-//	private boolean isScheduleInThePast(Schedule schedule) {
-//	Date date=new Date();
-//	
-//			return false;
-//	}
-
+	/**
+	 * method gets all schedules form the DB.
+	 * 
+	 * @return Collection<Schedule>
+	 */
 	public Collection<Schedule> getScheduels() {
 
 		return dao.getAll();
 	}
 
+	/**
+	 * Method adds schedules to the Db
+	 * 
+	 * @param schedule
+	 */
 	public long save(Schedule schedule) {
 
 		LOGGER.debug("Saving: " + schedule);
@@ -54,29 +72,57 @@ public class ScheduleService {
 
 	}
 
+	/**
+	 * Method delletss schedules to the Db
+	 * 
+	 * @param schedule
+	 */
 	public long delete(Schedule schedule) {
 		LOGGER.debug("Deleting: " + schedule);
-		return dao.delete(schedule);
 
+		return dao.delete(schedule);
 	}
 
+	/**
+	 * Method returns number of schedules form db.
+	 * 
+	 * @param id
+	 * @return int
+	 */
 	public int countSchedules(long id) {
 
 		return (dao.searchByStudentId(id).size() * 100 / 30);
 
 	}
 
+	/**
+	 * method updates schedule
+	 * 
+	 * @param schedule
+	 */
 	public void update(Schedule schedule) {
 
-		 dao.update(schedule);
+		dao.update(schedule);
 
 	}
 
+	/**
+	 * returnes schedules based on theacher id
+	 * 
+	 * @param teahcerId
+	 * @return Collection<Schedule>
+	 */
 	public Collection<Schedule> searchByTeacherId(long teahcerId) {
 
 		return dao.searchByTeacherId(teahcerId);
 	}
 
+	/**
+	 * returns scheduled based on student id
+	 * 
+	 * @param id
+	 * @return Collection<Schedule>
+	 */
 	public Collection<Schedule> searchByStudentId(int id) {
 
 		return dao.searchByStudentId(id);
@@ -118,6 +164,29 @@ public class ScheduleService {
 	public Collection<Schedule> searchByStudentId(long l, long teacherId) {
 
 		return dao.searchByStudentId(l, teacherId);
+	}
+
+	public boolean dateHasPased(Schedule schedule) {
+
+		Date date = new Date();
+
+		if (setTime(schedule).before(date)) {
+			return true;
+
+		}
+
+		return false;
+
+	}
+
+	private Date setTime(Schedule schedule) {
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(schedule.getDate());
+		cal.set(Calendar.HOUR_OF_DAY, schedule.getStartHour());
+		cal.set(Calendar.MINUTE, 0);
+		cal.set(Calendar.SECOND, 0);
+		cal.set(Calendar.MILLISECOND, 0);
+		return cal.getTime();
 	}
 
 }
