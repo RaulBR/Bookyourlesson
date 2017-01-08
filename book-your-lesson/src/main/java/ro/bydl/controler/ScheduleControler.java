92fcec2b45db6b0c70c36ea5626e240b155395d6
Raul Ranete <ranete.raul@yahoo.com>
2017-01-08 02:37:11 +0200

    no message

--- book-your-lesson/src/main/java/ro/bydl/controler/ScheduleControler.java
+++ book-your-lesson/src/main/java/ro/bydl/controler/ScheduleControler.java
@@ -1,11 +1,9 @@
 package ro.bydl.controler;
 
 import java.util.Calendar;
-import java.util.Collection;
 
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
-
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -13,16 +11,15 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
 
-import ro.bydl.domain.User;
 import ro.bydl.domain.Schedule;
 import ro.bydl.domain.Student;
 import ro.bydl.domain.Teacher;
+import ro.bydl.domain.User;
 import ro.bydl.service.ScheduleService;
 import ro.bydl.service.StudentService;
 import ro.bydl.service.TeacherService;
