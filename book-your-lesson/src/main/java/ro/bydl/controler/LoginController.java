0bc403c4f23f201f989f3c98933d8459f1ee09ac
Raul Ranete <ranete.raul@yahoo.com>
2017-01-27 22:26:56 +0200

    Login fix

--- book-your-lesson/src/main/java/ro/bydl/controler/LoginController.java
+++ book-your-lesson/src/main/java/ro/bydl/controler/LoginController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.SessionAttributes;
-import org.springframework.web.bind.support.SessionStatus;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
 
@@ -49,10 +48,10 @@ public class LoginController {
 	 * @return
 	 */
 	@RequestMapping("")
-	public ModelAndView mainPage(HttpSession session,  SessionStatus status) {
+	public ModelAndView mainPage(HttpSession session) {
 		
 		ModelAndView mav = new ModelAndView("login");
-
+		System.out.println(mav.toString());
 		
 		return mav;
 	}
